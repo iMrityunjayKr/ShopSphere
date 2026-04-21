a12061311fbe4eeb218e729334a288b1cdbd10de
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2026-04-21 23:55:54 +0530

    Refactor Navbar component for mobile responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,45 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/navbar.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import { FaShoppingCart } from "react-icons/fa";
-import { useLocation } from "react-router-dom";
+import { HiMenuAlt3, HiX } from "react-icons/hi";
 
 function Navbar() {
-  const { cartItems, searchItems, searchTerm, totalPrice } =
-    useContext(CartContext);
-
+  const { cartItems, searchItems, totalPrice } = useContext(CartContext);
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(false);
 
   return (
     <nav className="navbar">
       <div className="brand">
-        <img
-          src="/logo.png"
-          style={{ backgroundColor: "#2563eb" }}
-          className="h-4 w-4 object-contain"
-        />
+        <img src="/logo.png" alt="logo" className="logo-img" />
         <span className="app-name">ShopSphere</span>
       </div>
+
       {location.pathname === "/shop" && (
         <div className="search-bar">
           <input
             className="search-input"
             type="text"
             onChange={(e) => searchItems(e.target.value)}
-            placeholder="search any product"
+            placeholder="Search any product..."
           />
         </div>
       )}
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/cart" className="cart-link">
-          {/* Show price if cart isn't empty */}
+
+      <div className={`nav-links ${isMobile ? "nav-active" : ""}`}>
+        <Link to="/" onClick={() => setIsMobile(false)}>
+          Home
+        </Link>
+        <Link to="/shop" onClick={() => setIsMobile(false)}>
+          Shop
+        </Link>
+        <Link
+          to="/cart"
+          className="cart-link"
+          onClick={() => setIsMobile(false)}
+        >
           {cartItems.length > 0 && (
             <span className="nav-price">${totalPrice.toFixed(2)}</span>
           )}
-
           <div className="cart-icon-wrapper">
             <FaShoppingCart />
             {cartItems.length > 0 && (
@@ -47,6 +50,10 @@ function Navbar() {
             )}
           </div>
         </Link>
+      </div>
+
+      <div className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? <HiX /> : <HiMenuAlt3 />}
       </div>
     </nav>
   );
